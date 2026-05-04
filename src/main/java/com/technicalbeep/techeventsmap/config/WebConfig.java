02e2ab29974d407bf34cf8d166c0587b52d9bb92
oowais95 <45528844+oowais95@users.noreply.github.com>
2026-05-04 23:35:30 +0500

    integrated with back - running code

--- src/main/java/com/technicalbeep/techeventsmap/config/WebConfig.java
+++ src/main/java/com/technicalbeep/techeventsmap/config/WebConfig.java
@@ -13,9 +13,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/events/**")
+        registry.addMapping("/**")
                 .allowedOrigins(allowedOrigin)
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .maxAge(3600);
     }
